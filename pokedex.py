2828c3013cf4e45467a401931c6e93955bf92896
ivann-galic <ivann.galic@edu.itescia.fr>
2020-04-29 10:26:04 +0200

    fonction -info améliorée pour les évolutions

--- pokedex.py
+++ pokedex.py
@@ -23,24 +23,22 @@ def list_option(dictionary):
     print("")
 
 
-def info_option(dictionary):
-    pokemon_name = input("Please enter a pokemon:\n").title()
-    found = False
+def next_evo(dictionary, pokemon_name):
     for pokemon in dictionary:
         if pokemon['name'] == pokemon_name:
-            print("id: ", pokemon['pkdx_id'])
-            print("name: ", pokemon['name'])
-            print("description: ", "\n".join(textwrap.wrap(pokemon['description'], 160)))
-            if len(pokemon['types']) == 1:
-                print("type: ", pokemon['types'][0])
-            elif len(pokemon['types']) == 2:
-                print("types: ", pokemon['types'][0], "/", pokemon['types'][1])
             for key in pokemon.keys():
                 if key == 'evolutions':
                     try:
                         if len(pokemon['evolutions']) == 1:
-                            print("Evolves by", pokemon['evolutions'][0]['method'].replace("_", " "), "to",
-                                  pokemon['evolutions'][0]['to'])
+                            pokemons_next_evo = pokemon['evolutions'][0]['to']
+                            if pokemon['evolutions'][0]['method'] == 'level_up':
+                                print("Evolves by", pokemon['evolutions'][0]['method'].replace("_", " "), "at",
+                                      pokemon['evolutions'][0]['level'], "to", pokemon['evolutions'][0]['to'])
+                            else:
+                                print("Evolves by", pokemon['evolutions'][0]['method'].replace("_", " "), "to",
+                                      pokemon['evolutions'][0]['to'])
+                            next_evo(dictionary, pokemons_next_evo)
+
                         elif len(pokemon['evolutions']) > 1:
                             print("This pokemon can evolve to", len(pokemon['evolutions']), "different pokemon:")
                             i = 0
@@ -50,8 +48,26 @@ def info_option(dictionary):
                                 i += 1
                     except IndexError:
                         continue
-        if not found:
-            print("Error: unknown pokemon")
+
+
+def info_option(dictionary):
+    found = False
+    pokemon_name = input("Please enter a pokemon:\n").title()
+    for pokemon in dictionary:
+        if pokemon['name'] == pokemon_name:
+            found = True
+            print("id: ", pokemon['pkdx_id'])
+            print("name: ", pokemon['name'])
+            print("description: ", "\n".join(textwrap.wrap(pokemon['description'], 180)))
+            if len(pokemon['types']) == 1:
+                print("type: ", pokemon['types'][0])
+            elif len(pokemon['types']) == 2:
+                print("types: ", pokemon['types'][0], "/", pokemon['types'][1])
+            for key in pokemon.keys():
+                if key == 'evolutions':
+                    next_evo(dictionary, pokemon_name)
+    if not found:
+        print("Error: unknown pokemon")
     print("")
 
 
@@ -60,8 +76,8 @@ def description_option(dictionary):
     found = False
     for pokemon in dictionary:
         if pokemon['name'] == pokemon_name:
-            print("\n".join(textwrap.wrap(pokemon['description'], 160)))
             found = True
+            print("\n".join(textwrap.wrap(pokemon['description'], 160)))
     if not found:
         print("Error: unknown pokemon")
     print("")
@@ -72,6 +88,7 @@ def type_option(dictionary):
     found = False
     for pokemon in dictionary:
         if pokemon['name'] == pokemon_name:
+            found = True
             if len(pokemon['types']) == 1:
                 print("type: ", pokemon['types'][0])
             elif len(pokemon['types']) == 2:
