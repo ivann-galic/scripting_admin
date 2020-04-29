5f33fb95c621c43143b7cc46656a4a6e6994cd6d
ivann-galic <ivann.galic@edu.itescia.fr>
2020-04-29 09:16:50 +0200

    -info fonctionne, à voir pour le argparse

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import textwrap
+import argparse
 
 
 def help_option():
@@ -19,38 +20,30 @@ def list_option(dictionary):
     print("")
 
 
-def test(dictionary):
-    for pokemon in dictionary:
-        print("id: ", pokemon['pkdx_id'])
-        print("name: ", pokemon['name'])
-        print("description: ", "\n".join(textwrap.wrap(pokemon['description'], 160)))
-        if len(pokemon['types']) == 1:
-            print("type: ", pokemon['types'][0])
-        elif len(pokemon['types']) == 2:
-            print("types: ", pokemon['types'][0], "/", pokemon['types'][1])
-        for key in pokemon.keys():
-            if key == 'evolutions':
-                try:
-                    if len(pokemon['evolutions']) == 1:
-                        print("Evolves by", pokemon['evolutions'][0]['method'].replace("_", " "), "to", pokemon['evolutions'][0]['to'])
-                    elif len(pokemon['evolutions']) > 1:
-                        print("This pokemon can evolve to", len(pokemon['evolutions']), "different pokemon:")
-                        i = 0
-                        while i < len(pokemon['evolutions']):
-                            print("Evolves by", pokemon['evolutions'][i]['method'].replace("_", " "), "to", pokemon['evolutions'][i]['to'])
-                            i += 1
-                except IndexError:
-                    continue
-    print("")
-
-
 def info_option(dictionary):
-    pokemon_name = input("Please enter a pokemon:\n").lower()
+    pokemon_name = input("Please enter a pokemon:\n").title()
     for pokemon in dictionary:
         if pokemon['name'] == pokemon_name:
-            print(pokemon['pkdx_id'], pokemon['name'])
-        else:
-            print("echec")
+            print("id: ", pokemon['pkdx_id'])
+            print("name: ", pokemon['name'])
+            print("description: ", "\n".join(textwrap.wrap(pokemon['description'], 180)))
+            if len(pokemon['types']) == 1:
+                print("type: ", pokemon['types'][0])
+            elif len(pokemon['types']) == 2:
+                print("types: ", pokemon['types'][0], "/", pokemon['types'][1])
+            for key in pokemon.keys():
+                if key == 'evolutions':
+                    try:
+                        if len(pokemon['evolutions']) == 1:
+                            print("Evolves by", pokemon['evolutions'][0]['method'].replace("_", " "), "to", pokemon['evolutions'][0]['to'])
+                        elif len(pokemon['evolutions']) > 1:
+                            print("This pokemon can evolve to", len(pokemon['evolutions']), "different pokemon:")
+                            i = 0
+                            while i < len(pokemon['evolutions']):
+                                print("Evolves by", pokemon['evolutions'][i]['method'].replace("_", " "), "to", pokemon['evolutions'][i]['to'])
+                                i += 1
+                    except IndexError:
+                        continue
     print("")
 
 
@@ -71,8 +64,6 @@ def commands(data_dict):
             exit_option()
         elif user_input == "info":
             info_option(data_dict)
-        elif user_input == "test":
-            test(data_dict)
         else:
             print("Invalid command")
             help_option()
