40f62fe327ec5c0d267e8f5c24652fbf1bddcd05
ivann-galic <ivann.galic@edu.itescia.fr>
2020-04-30 10:08:10 +0200

    command -weakness -resistance -picture

--- pokedex.py
+++ pokedex.py
@@ -2,6 +2,9 @@ import json
 import sys
 import textwrap
 import argparse
+import urllib
+from urllib.request import urlopen
+from PIL import Image
 
 
 def json_init():
@@ -48,6 +51,7 @@ def next_evo(dictionary, pokemon_name):
 def info_option(pokemon_name):
     dictionary = json_init()
     found = False
+
     for pokemon in dictionary:
         if pokemon['name'] == pokemon_name:
             found = True
@@ -66,22 +70,144 @@ def info_option(pokemon_name):
     print("")
 
 
-def commands(user_input, arg2):
-    if user_input == "help":
-        help_option()
-    elif user_input == "list":
-        list_option()
-    elif user_input == "exit":
-        exit_option()
-    elif user_input == "info":
-        info_option(arg2)
-    elif user_input == "descript":
-        description_option(arg2)
-    elif user_input == "type":
-        type_option(arg2)
-    else:
-        print("Invalid command")
-        help_option()
+def get_weakness(type_name):
+    if type_name == "bug":
+        return ['fire', 'flying', 'rock']
+    elif type_name == "dragon":
+        return ['ice', 'dragon', 'fairy']
+    elif type_name == "electric":
+        return ['ground']
+    elif type_name == "fairy":
+        return ['poison', 'steel']
+    elif type_name == "fighting":
+        return ['flying', 'psychic', 'fairy']
+    elif type_name == "fire":
+        return ['water', 'ground', 'rock']
+    elif type_name == "flying":
+        return ['electric', 'ice', 'rock']
+    elif type_name == "ghost":
+        return ["ghost"]
+    elif type_name == "grass":
+        return ['fire', 'ice', 'poison', 'flying', 'bug']
+    elif type_name == "ground":
+        return ['water', 'grass', 'ice']
+    elif type_name == "ice":
+        return ['fire', 'fighting', 'rock', 'steel']
+    elif type_name == "normal":
+        return ["fighting"]
+    elif type_name == "poison":
+        return ['ground', 'psychic']
+    elif type_name == "psychic":
+        return ['bug', 'ghost']
+    elif type_name == "rock":
+        return ['water', 'grass', 'fighting', 'ground', 'steel']
+    elif type_name == "steel":
+        return ['fire', 'fighting', 'ground']
+    elif type_name == "water":
+        return ['grass', 'electric']
+
+
+def get_resistance(type_name):
+    if type_name == "bug":
+        return ['grass', 'fighting', 'ground']
+    elif type_name == "dragon":
+        return ['fire', 'water', 'grass', 'electric']
+    elif type_name == "electric":
+        return ['electric', 'flying']
+    elif type_name == "fairy":
+        return ['fighting', 'bug']
+    elif type_name == "fighting":
+        return ['bug', 'rock']
+    elif type_name == "fire":
+        return ['fire', 'grass', 'ice', 'bug', 'steel', 'fairy']
+    elif type_name == "flying":
+        return ['grass', 'fighting', 'bug']
+    elif type_name == "ghost":
+        return ["poison", 'bug']
+    elif type_name == "grass":
+        return ['water', 'grass', 'electric', 'ground']
+    elif type_name == "ground":
+        return ['poison', 'rock']
+    elif type_name == "ice":
+        return ['ice']
+    elif type_name == "normal":
+        return []
+    elif type_name == "poison":
+        return ['grass', 'fighting', 'poison', 'bug', 'fairy']
+    elif type_name == "psychic":
+        return ['fighting', 'psychic']
+    elif type_name == "rock":
+        return ['normal', 'fire', 'poison', 'flying']
+    elif type_name == "steel":
+        return ['normal', 'grass', 'ice', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'steel', 'fairy', ]
+    elif type_name == "water":
+        return ['fire', 'water' 'ice', 'steel']
+
+
+def weakness_option(pokemon_name):
+    dictionary = json_init()
+    found = False
+    list_to_convert = []
+    scd_list_to_convert = []
+
+    for pokemon in dictionary:
+        if pokemon['name'] == pokemon_name:
+            result = "is weak to:"
+            found = True
+            if len(pokemon['types']) == 1:
+                list_to_convert = get_weakness(pokemon['types'][0])
+                for element in list_to_convert:
+                    result = result + " " + element
+            elif len(pokemon['types']) == 2:
+                list_to_convert = get_weakness(pokemon['types'][0])
+                scd_list_to_convert = get_weakness(pokemon['types'][1])
+                list_wo_duplicate = list(set(list_to_convert + scd_list_to_convert))
+                for element in list_wo_duplicate:
+                    result = result + " " + element
+            print(pokemon['name'], result)
+    if not found:
+        print("Error: unknown pokemon")
+    print("")
+
+
+def resistance_option(pokemon_name):
+    dictionary = json_init()
+    found = False
+    list_to_convert = []
+    scd_list_to_convert = []
+
+    for pokemon in dictionary:
+        if pokemon['name'] == pokemon_name:
+            result = "is resisting:"
+            found = True
+            if len(pokemon['types']) == 1:
+                list_to_convert = get_resistance(pokemon['types'][0])
+                for element in list_to_convert:
+                    result = result + " " + element
+            elif len(pokemon['types']) == 2:
+                list_to_convert = get_resistance(pokemon['types'][0])
+                scd_list_to_convert = get_resistance(pokemon['types'][1])
+                list_wo_duplicate = list(set(list_to_convert + scd_list_to_convert))
+                for element in list_wo_duplicate:
+                    result = result + " " + element
+            print(pokemon['name'], result)
+    if not found:
+        print("Error: unknown pokemon")
+    print("")
+
+
+def picture_option(pokemon_name):
+    dictionary = json_init()
+    found = False
+
+    for pokemon in dictionary:
+        if pokemon['name'] == pokemon_name:
+            found = True
+            img = Image.open(urlopen(pokemon['image_url']))
+            img.show()
+    if not found:
+        print("Error: unknown pokemon")
+    print("")
 
 
 def main():
@@ -93,6 +219,12 @@ def main():
     group.add_argument("-l", "--list", help="to access the list of all pokemon.", action="store_true")
     group.add_argument("-i", "--info",
                        help="to access the data about an individual pokemon. Needs to be followed by a pokemon name.")
+    group.add_argument("-w", "--weakness",
+                       help="to access the weaknesses of an individual pokemon. Needs to be followed by a pokemon name.")
+    group.add_argument("-r", "--resistance",
+                       help="to access the resistances of an individual pokemon. Needs to be followed by a pokemon name.")
+    group.add_argument("-p", "--picture",
+                       help="to display the picture of an individual pokemon in browser. Needs to be followed by a pokemon name.")
 
     arguments = parser.parse_args()
 
@@ -100,6 +232,12 @@ def main():
         list_option()
     elif arguments.info is not None:
         info_option(arguments.info.title())
+    elif arguments.weakness is not None:
+        weakness_option(arguments.weakness.title())
+    elif arguments.resistance is not None:
+        resistance_option(arguments.resistance.title())
+    elif arguments.picture is not None:
+        picture_option(arguments.picture.title())
 
 
 if __name__ == '__main__':
